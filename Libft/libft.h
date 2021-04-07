443e5958a6a67d707c352f4c35e01b29f5bdf111
dalvaro74 <dalvaro74gmail.com>
2021-04-07 20:21:28 +0200

    Ordenado el Makefile y .h

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: dalvaro- <dalvaro-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 19:36:11 by dalvaro-          #+#    #+#             */
-/*   Updated: 2021/04/07 19:36:22 by dalvaro-         ###   ########.fr       */
+/*   Updated: 2021/04/07 20:18:51 by dalvaro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,19 @@ typedef struct s_list
 }				t_list;
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
-int		ft_isascii(int c);
 int		ft_isdigit(int c);
-int		ft_isspace(int c);
 int		ft_isprint(int c);
-int		ft_tolower(int c);
+int		ft_isascii(int c);
 int		ft_toupper(int c);
+int		ft_tolower(int c);
 int		ft_atoi(const char *s);
 size_t	ft_strlen(const char *str);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
