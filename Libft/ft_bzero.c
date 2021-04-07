356d54fddb6763f980b888886f22208ed0ecdc8c
dalvaro74 <dalvaro74gmail.com>
2021-04-07 21:07:10 +0200

    inclluidas cabeceras y prototipos

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -6,7 +6,13 @@
 /*   By: dalvaro- <dalvaro-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 19:41:49 by dalvaro-          #+#    #+#             */
-/*   Updated: 2021/04/07 19:41:50 by dalvaro-         ###   ########.fr       */
+/*   Updated: 2021/04/07 20:23:10 by dalvaro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	
+}
