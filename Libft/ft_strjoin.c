f587c01f5dd0d887f7a56a89996169f1e034e801
dalvaro74 <dalvaro74gmail.com>
2021-04-14 17:08:26 +0200

    strjoin

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,13 +6,52 @@
 /*   By: dalvaro- <dalvaro-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 20:55:01 by dalvaro-          #+#    #+#             */
-/*   Updated: 2021/04/07 20:55:17 by dalvaro-         ###   ########.fr       */
+/*   Updated: 2021/04/14 17:03:42 by dalvaro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*ptr;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	ptr = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen(s1))
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < ft_strlen(s2))
+	{
+		ptr[i + j] = s2[j];
+		j++;
+	}
+	ptr[i + j] = '\0';
+	return (ptr);
+}
+*/
 
 #include "libft.h"
 
 char	*ft_strjoin(const char *s1, const char *s2)
 {
-	
+	char	*ptr;
+
+	if (!s1 || !s2)
+		return (NULL);
+	ptr = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (ptr)
+	{
+		ft_memcpy(ptr, s1, ft_strlen(s1));
+		ft_memcpy(ptr + ft_strlen(s1), s2, ft_strlen(s2) + 1);
+	}
+	return (ptr);
 }
